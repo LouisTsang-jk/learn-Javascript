7117d8763272e053555177e9821368e56baaaafa
LouisTsang <wx_05e14399142e4c6384cdaf3989194300@git.code.tencent.com>
2019-12-01 00:33:15 +0800

    pattern

--- design-pattern/singleton-pattern.js
+++ design-pattern/singleton-pattern.js
@@ -16,3 +16,25 @@ v1.test = "test";
 const v2 = Unique.getTheO();
 v2.test === v1.test;
 // => true
+
+/**
+ * scene: Storage
+ */
+class Storage {
+  static getInstance() {
+    if (!Storage.instance) {
+      Storage.instance = new Storage();
+    }
+    return Storage.instance;
+  }
+  setItem(key, value) {
+    localStorage.setItem(key, value);
+  }
+  getItem(key) {
+    localStorage.getItem(key);
+  }
+}
+
+/**
+ * scene: Modal (global)
+ */
