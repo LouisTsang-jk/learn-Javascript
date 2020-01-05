5a1e065bb2caed6219f532471c88102dd1765b12
LouisTsang <wx_05e14399142e4c6384cdaf3989194300@git.code.tencent.com>
2020-01-06 00:02:36 +0800

    add reg

---
+++ lodash/get/get.js
@@ -0,0 +1,40 @@
+'use strict'
+const object = { 'a': [{ 'b': { 'c': 3 } }] };
+const path1 = 'a[0].b.c';           // normal path
+const path2 = ['a', '0', 'b', 'c']; // arr path
+const path3 = ['a', '0', 'b', 'd']; // error arr path
+const path4 = 'a[0].b.c["abc"].d';  // custom path
+const result = get(object, path4);
+console.log(result);
+function get(object, path) {
+  if (object === undefined) {
+    return undefined
+  }
+  let result = object;
+  // path is array
+  if (Array.isArray(path)) {
+    let num = 0
+    while (path.length > 0) {
+      num++;
+      result = result[path[0]];
+      if (result === undefined) {
+        return undefined;
+      }
+      path.shift();
+    }
+    return result;
+  }
+  // path is not a path
+  const paths = path.split(".");
+  const reg = /(.+?)\[(.+?)\]/g;
+  console.log(paths);
+  paths.forEach((item) => {
+    if (reg.test(item)) {
+      let temp = reg.exec(item);
+      console.log(temp)
+    };
+  })
+  // let keys = path.match(reg);
+  // console.log(keys);
+
+}
