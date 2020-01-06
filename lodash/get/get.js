54315a721c25dabdcd6df1ad0613a6d8d5cd7af2
LouisTsang <646168919@qq.com>
2020-01-06 22:48:20 +0800

    lodash get

--- lodash/get/get.js
+++ lodash/get/get.js
@@ -1,37 +1,54 @@
 'use strict'
-const object = { 'a': [{ 'b': { 'c': 3 } }] };
+const object1 = { 'a': [{ 'b': { 'c': 3 } }] };
+const object2 = { 'a': [{ 'b': { 'c': { 'abc': { 'd': 999 } } } }] };
 const path1 = 'a[0].b.c';           // normal path
 const path2 = ['a', '0', 'b', 'c']; // arr path
 const path3 = ['a', '0', 'b', 'd']; // error arr path
 const path4 = 'a[0].b.c["abc"].d';  // custom path
-const result = get(object, path4);
-// console.log(result);
+
+const result = get(object2, path4);
+console.log(result);
 function get(object, path) {
   if (object === undefined) {
     return undefined
   }
-  let result = object;
+  let result;
   // path is array
   if (Array.isArray(path)) {
-    let num = 0
-    while (path.length > 0) {
-      num++;
-      result = result[path[0]];
-      if (result === undefined) {
-        return undefined;
-      }
-      path.shift();
-    }
-    return result;
+    result = cycleGetValue(object, path);
   }
   // path is not a path
   const paths = path.split(".");
   const regKeys = /(.+?)\[(.+?)\]/g;
-  console.log(paths);
-  paths.forEach((item) => {
-    if (regKeys.test(item)) {
-      console.log(item);
-
+  result = paths.map((item, index) => {
+    const isInclude = regKeys.test(item);
+    // lastIndex is change after (test or exec)
+    regKeys.lastIndex = 0;
+    if (isInclude) {
+      const itemMatch = [...item.matchAll(regKeys)];
+      return [itemMatch[0][1], itemMatch[0][2]];
     };
+    return item;
   })
+  // flat array
+  result = cycleGetValue(object, result.flat());
+  return result;
+}
+
+// path -> array
+function cycleGetValue(object, path) {
+  while (path.length > 0) {
+    // quotes filter(remove quotes)
+    let key = path[0];
+    const regQuote = /(?:\"(.+?)\")|(?:\'(.+?)\')/;
+    if (regQuote.test(key)) {
+      key = key.replace(regQuote, '$1');
+    }
+    object = object[key];
+    if (object === undefined) {
+      return undefined;
+    }
+    path.shift();
+  }
+  return object;
 }
