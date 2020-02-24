bb26060b4d7ada8c11eb308a637ecab11d728700
LouisTsang <646168919@qq.com>
2020-02-24 23:44:52 +0800

    gulp

---
+++ gulp/vue-router/gulpfile.js
@@ -0,0 +1,14 @@
+const { series, src, dest } = require('gulp');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+
+const config = {
+  
+}
+
+exports.default = function () {
+  return src('file.js')
+    .pipe(replace(/import (?!Vue|Router)(.+) from '(.+)'/g,`const $1 = ()=> import('$2')`))
+    .pipe(rename({ extname: '.lazy.js' }))
+    .pipe(dest('lazy/'));
+}
