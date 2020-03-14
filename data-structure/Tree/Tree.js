c252701949504bc9bdbd46cc4d682dfba9d52641
LouisTsang <646168919@qq.com>
2020-03-14 22:29:54 +0800

    tree

--- data-structure/Tree/Tree.js
+++ data-structure/Tree/Tree.js
@@ -1,5 +1,21 @@
-function TreeNode(key){
+function TreeNode(key) {
   this.key = key;
   this.left = null;
   this.right = null;
+}
+
+// DLR(前序遍历)、
+// LDR(中序遍历)、
+// LRD(后序遍历)
+
+function Tree({ DLR, LDR, LRD }) {
+  // 必须有中序
+  if (!LDR) {
+    throw Error('LDR must exist');
+  }
+  // 前序，中序必须有一个
+  if (DLR || LRD) {
+    throw Error('DLR and LRD must have one');
+  }
+
 }
