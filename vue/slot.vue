ec7e18dbfdcb424083c3252300962258e915cbc5
LouisTsang <646168919@qq.com>
2020-06-16 23:54:44 +0800

    Add:slot

---
+++ vue/slot.vue
@@ -0,0 +1,26 @@
+# v-slot(缩写:`#`)
+## 用法
+1.```
+<!-- 具名插槽 -->
+<base-layout>
+  <template v-slot:header>
+    Header content
+  </template>
+  Default slot content
+  <template v-slot:footer>
+    Footer content
+  </template>
+</base-layout>
+<!-- 接收 prop 的具名插槽 -->
+<infinite-scroll>
+  <template v-slot:item="slotProps">
+    <div class="item">
+      {{ slotProps.item.text }}
+    </div>
+  </template>
+</infinite-scroll>
+<!-- 接收 prop 的默认插槽，使用了解构 -->
+<mouse-position v-slot="{ x, y }">
+  Mouse position: {{ x }}, {{ y }}
+</mouse-position>
+````
